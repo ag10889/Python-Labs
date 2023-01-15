712349aab123150eb61ce138c4b9bfb62962d446
ag10889 <122389120+ag10889@users.noreply.github.com>
2023-01-14 19:22:17 -0500

    Add files via upload

---
+++ Class1Tester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace lab1C
+{
+    public static void Main(string[] args)
+    {
+        char[,] carArray = Class1.make_forward();
+        for (int row = 0;row<4;row++)
+        {
+            for (int column = 0; column < carArray[row].Length; column++)
+            {
+             Console.Write(carArray[row, column]);
+            }
+        }
+    }
+}
+
