69ca806a6ba52c7ca6341a4ea31f54f52bcfde46
ag10889 <122389120+ag10889@users.noreply.github.com>
2023-01-17 11:29:17 -0500

    Update Class1Tester.cs

--- Class1Tester.cs
+++ Class1Tester.cs
@@ -4,7 +4,7 @@ namespace lab1C
     public static void Main(string[] args)
     {
         char[,] carArray = Class1.make_forward();
-        for (int row = 0;row<4;row++)
+        for (int row = 0;row<carArray.Length;row++)
         {
             for (int column = 0; column < carArray[row].Length; column++)
             {
